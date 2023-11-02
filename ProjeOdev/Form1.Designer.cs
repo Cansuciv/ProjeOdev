8c77399a9a4a31ca324dc6268d26fcf2b566464e
CANSU ÇİV <cansuciv@posta.mu.edu.tr>
2023-11-02 18:42:10 +0300

    Add project files.

---
+++ ProjeOdev/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ProjeOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            GirisYapButonu = new Button();
+            button4 = new Button();
+            Kullanıcı_girişi = new Button();
+            Yetkili_polis_girişi = new Button();
+            Admin_Girişi = new Button();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(728, 455);
+            button2.Name = "button2";
+            button2.Size = new Size(223, 73);
+            button2.TabIndex = 55;
+            button2.Text = "ÜYE OL";
+            button2.UseVisualStyleBackColor = true;
+            button2.Visible = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(728, 360);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 73);
+            button1.TabIndex = 54;
+            button1.Text = "GİRİŞ YAP";
+            button1.UseVisualStyleBackColor = true;
+            button1.Visible = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(284, 455);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(279, 43);
+            textBox2.TabIndex = 53;
+            textBox2.Visible = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(284, 390);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(279, 43);
+            textBox1.TabIndex = 52;
+            textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(151, 443);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 46);
+            label2.TabIndex = 51;
+            label2.Text = "Şifre : ";
+            label2.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(50, 387);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 46);
+            label1.TabIndex = 50;
+            label1.Text = "Kullanıcı adı : ";
+            label1.Visible = false;
+            // 
+            // GirisYapButonu
+            // 
+            GirisYapButonu.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            GirisYapButonu.Location = new Point(237, 539);
+            GirisYapButonu.Name = "GirisYapButonu";
+            GirisYapButonu.Size = new Size(158, 70);
+            GirisYapButonu.TabIndex = 49;
+            GirisYapButonu.Text = "GİRİŞ YAP";
+            GirisYapButonu.UseVisualStyleBackColor = true;
+            GirisYapButonu.Visible = false;
+            GirisYapButonu.Click += GirisYapButonu_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(947, 590);
+            button4.Name = "button4";
+            button4.Size = new Size(147, 67);
+            button4.TabIndex = 48;
+            button4.Text = "KAPAT";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Kullanıcı_girişi
+            // 
+            Kullanıcı_girişi.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            Kullanıcı_girişi.Location = new Point(765, 151);
+            Kullanıcı_girişi.Name = "Kullanıcı_girişi";
+            Kullanıcı_girişi.Size = new Size(299, 167);
+            Kullanıcı_girişi.TabIndex = 47;
+            Kullanıcı_girişi.Text = "KULLANICI GİRİŞİ";
+            Kullanıcı_girişi.UseVisualStyleBackColor = true;
+            Kullanıcı_girişi.Click += Kullanıcı_girişi_Click;
+            // 
+            // Yetkili_polis_girişi
+            // 
+            Yetkili_polis_girişi.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            Yetkili_polis_girişi.Location = new Point(390, 151);
+            Yetkili_polis_girişi.Name = "Yetkili_polis_girişi";
+            Yetkili_polis_girişi.Size = new Size(299, 167);
+            Yetkili_polis_girişi.TabIndex = 46;
+            Yetkili_polis_girişi.Text = "YETKİLİ POLİS GİRİŞİ";
+            Yetkili_polis_girişi.UseVisualStyleBackColor = true;
+            Yetkili_polis_girişi.Click += Yetkili_polis_girişi_Click;
+            // 
+            // Admin_Girişi
+            // 
+            Admin_Girişi.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            Admin_Girişi.Location = new Point(36, 151);
+            Admin_Girişi.Name = "Admin_Girişi";
+            Admin_Girişi.Size = new Size(299, 167);
+            Admin_Girişi.TabIndex = 45;
+            Admin_Girişi.Text = "ADMİN GİRİŞİ";
+            Admin_Girişi.UseVisualStyleBackColor = true;
+            Admin_Girişi.Click += Admin_Girişi_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1127, 712);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(GirisYapButonu);
+            Controls.Add(button4);
+            Controls.Add(Kullanıcı_girişi);
+            Controls.Add(Yetkili_polis_girişi);
+            Controls.Add(Admin_Girişi);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private Button GirisYapButonu;
+        private Button button4;
+        private Button Kullanıcı_girişi;
+        private Button Yetkili_polis_girişi;
+        private Button Admin_Girişi;
+    }
+}
---
+++ ProjeOdev/Form1.cs
@@ -0,0 +1,82 @@
+namespace ProjeOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Admin_Giriþi_Click(object sender, EventArgs e)
+        {
+            Kullanýcý_giriþi.Top = 20;
+            Kullanýcý_giriþi.Left = 400;
+
+            Admin_Giriþi.Visible = false;
+            Yetkili_polis_giriþi.Visible = false;
+
+
+            button1.Visible = true;
+            button2.Visible = true;
+
+            button1.Location = new Point(450, 300);
+            button2.Location = new Point(450, 430);
+        }
+
+        private void GirisYapButonu_Click(object sender, EventArgs e)
+        {
+            Form2 AdminGiris = new Form2();
+            AdminGiris.Show();
+        }
+
+        private void Yetkili_polis_giriþi_Click(object sender, EventArgs e)
+        {
+            Yetkili_polis_giriþi.Top = 20;
+            Yetkili_polis_giriþi.Left = 400;
+
+            Admin_Giriþi.Visible = false;
+            Kullanýcý_giriþi.Visible = false;
+
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            GirisYapButonu.Visible = true;
+
+            label1.Location = new Point(350, 300);
+            label2.Location = new Point(350, 400);
+            textBox1.Location = new Point(560, 300);
+            textBox2.Location = new Point(560, 400);
+            GirisYapButonu.Location = new Point(500, 500);
+
+            //Form3 PolisGiris = new Form3();
+            //PolisGiris.Show();
+        }
+
+        private void Kullanýcý_giriþi_Click(object sender, EventArgs e)
+        {
+            Kullanýcý_giriþi.Top = 20;
+            Kullanýcý_giriþi.Left = 400;
+
+            Admin_Giriþi.Visible = false;
+            Yetkili_polis_giriþi.Visible = false;
+
+
+            button1.Visible = true;
+            button2.Visible = true;
+
+            button1.Location = new Point(450, 300);
+            button2.Location = new Point(450, 430);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
