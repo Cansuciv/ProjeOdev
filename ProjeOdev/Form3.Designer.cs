3f6ae466e911fe986d1cc0055a5a47b03fd40e24
CANSU ÇİV <cansuciv@posta.mu.edu.tr>
2023-11-03 14:12:24 +0300

    Form3 de Yetkili Polis Kontroller

--- ProjeOdev/Form3.Designer.cs
+++ ProjeOdev/Form3.Designer.cs
@@ -40,6 +40,22 @@
             labelPlaka = new Label();
             SorgulaButton = new Button();
             label1 = new Label();
+            Sehir = new Label();
+            Ilce = new Label();
+            Plaka = new Label();
+            CezaTuru = new Label();
+            CezaMiktarı = new Label();
+            CezaGirişTarihi = new Label();
+            CezaSonÖdemeTarihi = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            EkleButton = new Button();
+            CezaSilButton = new Button();
             SuspendLayout();
             // 
             // CezaBilgisiOgren
@@ -62,6 +78,7 @@
             CezaSil.TabIndex = 51;
             CezaSil.Text = "Ceza Sil";
             CezaSil.UseVisualStyleBackColor = true;
+            CezaSil.Click += CezaSil_Click;
             // 
             // CezaEkle
             // 
@@ -83,11 +100,12 @@
             button4.TabIndex = 53;
             button4.Text = "KAPAT";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(269, 460);
+            textBox1.Location = new Point(241, 369);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(235, 39);
             textBox1.TabIndex = 68;
@@ -97,7 +115,7 @@
             // 
             labelCezaSonOdemeTarihi.AutoSize = true;
             labelCezaSonOdemeTarihi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelCezaSonOdemeTarihi.Location = new Point(161, 565);
+            labelCezaSonOdemeTarihi.Location = new Point(133, 474);
             labelCezaSonOdemeTarihi.Name = "labelCezaSonOdemeTarihi";
             labelCezaSonOdemeTarihi.Size = new Size(65, 28);
             labelCezaSonOdemeTarihi.TabIndex = 67;
@@ -108,7 +126,7 @@
             // 
             labelCezaGirisTarihi.AutoSize = true;
             labelCezaGirisTarihi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelCezaGirisTarihi.Location = new Point(161, 516);
+            labelCezaGirisTarihi.Location = new Point(133, 425);
             labelCezaGirisTarihi.Name = "labelCezaGirisTarihi";
             labelCezaGirisTarihi.Size = new Size(65, 28);
             labelCezaGirisTarihi.TabIndex = 66;
@@ -119,7 +137,7 @@
             // 
             labelCezaMiktari.AutoSize = true;
             labelCezaMiktari.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelCezaMiktari.Location = new Point(53, 606);
+            labelCezaMiktari.Location = new Point(25, 515);
             labelCezaMiktari.Name = "labelCezaMiktari";
             labelCezaMiktari.Size = new Size(65, 28);
             labelCezaMiktari.TabIndex = 65;
@@ -130,7 +148,7 @@
             // 
             labelCezaTuru.AutoSize = true;
             labelCezaTuru.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelCezaTuru.Location = new Point(53, 565);
+            labelCezaTuru.Location = new Point(25, 474);
             labelCezaTuru.Name = "labelCezaTuru";
             labelCezaTuru.Size = new Size(65, 28);
             labelCezaTuru.TabIndex = 64;
@@ -141,7 +159,7 @@
             // 
             labelPlaka.AutoSize = true;
             labelPlaka.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelPlaka.Location = new Point(53, 516);
+            labelPlaka.Location = new Point(25, 425);
             labelPlaka.Name = "labelPlaka";
             labelPlaka.Size = new Size(65, 28);
             labelPlaka.TabIndex = 63;
@@ -151,7 +169,7 @@
             // SorgulaButton
             // 
             SorgulaButton.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            SorgulaButton.Location = new Point(295, 584);
+            SorgulaButton.Location = new Point(267, 493);
             SorgulaButton.Name = "SorgulaButton";
             SorgulaButton.Size = new Size(163, 50);
             SorgulaButton.TabIndex = 62;
@@ -164,18 +182,191 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(34, 460);
+            label1.Location = new Point(6, 369);
             label1.Name = "label1";
             label1.Size = new Size(220, 37);
             label1.TabIndex = 61;
             label1.Text = "Plaka Numarası : ";
             label1.Visible = false;
             // 
+            // Sehir
+            // 
+            Sehir.AutoSize = true;
+            Sehir.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Sehir.Location = new Point(620, 380);
+            Sehir.Name = "Sehir";
+            Sehir.Size = new Size(70, 28);
+            Sehir.TabIndex = 69;
+            Sehir.Text = "Şehir : ";
+            Sehir.Visible = false;
+            // 
+            // Ilce
+            // 
+            Ilce.AutoSize = true;
+            Ilce.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Ilce.Location = new Point(635, 425);
+            Ilce.Name = "Ilce";
+            Ilce.Size = new Size(55, 28);
+            Ilce.TabIndex = 70;
+            Ilce.Text = "İlçe : ";
+            Ilce.Visible = false;
+            // 
+            // Plaka
+            // 
+            Plaka.AutoSize = true;
+            Plaka.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Plaka.Location = new Point(620, 463);
+            Plaka.Name = "Plaka";
+            Plaka.Size = new Size(72, 28);
+            Plaka.TabIndex = 71;
+            Plaka.Text = "Plaka : ";
+            Plaka.Visible = false;
+            // 
+            // CezaTuru
+            // 
+            CezaTuru.AutoSize = true;
+            CezaTuru.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            CezaTuru.Location = new Point(581, 507);
+            CezaTuru.Name = "CezaTuru";
+            CezaTuru.Size = new Size(111, 28);
+            CezaTuru.TabIndex = 72;
+            CezaTuru.Text = "Ceza Türü : ";
+            CezaTuru.Visible = false;
+            // 
+            // CezaMiktarı
+            // 
+            CezaMiktarı.AutoSize = true;
+            CezaMiktarı.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            CezaMiktarı.Location = new Point(563, 554);
+            CezaMiktarı.Name = "CezaMiktarı";
+            CezaMiktarı.Size = new Size(129, 28);
+            CezaMiktarı.TabIndex = 73;
+            CezaMiktarı.Text = "Ceza Miktarı :";
+            CezaMiktarı.Visible = false;
+            // 
+            // CezaGirişTarihi
+            // 
+            CezaGirişTarihi.AutoSize = true;
+            CezaGirişTarihi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            CezaGirişTarihi.Location = new Point(535, 599);
+            CezaGirişTarihi.Name = "CezaGirişTarihi";
+            CezaGirişTarihi.Size = new Size(157, 28);
+            CezaGirişTarihi.TabIndex = 74;
+            CezaGirişTarihi.Text = "Ceza Giriş Tarihi :";
+            CezaGirişTarihi.Visible = false;
+            // 
+            // CezaSonÖdemeTarihi
+            // 
+            CezaSonÖdemeTarihi.AutoSize = true;
+            CezaSonÖdemeTarihi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            CezaSonÖdemeTarihi.Location = new Point(471, 646);
+            CezaSonÖdemeTarihi.Name = "CezaSonÖdemeTarihi";
+            CezaSonÖdemeTarihi.Size = new Size(221, 28);
+            CezaSonÖdemeTarihi.TabIndex = 75;
+            CezaSonÖdemeTarihi.Text = "Ceza Son Ödeme Tarihi :";
+            CezaSonÖdemeTarihi.Visible = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(696, 384);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 27);
+            textBox2.TabIndex = 76;
+            textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(696, 429);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(166, 27);
+            textBox3.TabIndex = 77;
+            textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(696, 467);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 27);
+            textBox4.TabIndex = 78;
+            textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(696, 511);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(166, 27);
+            textBox5.TabIndex = 79;
+            textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(698, 558);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(166, 27);
+            textBox6.TabIndex = 80;
+            textBox6.Visible = false;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(698, 603);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(166, 27);
+            textBox7.TabIndex = 81;
+            textBox7.Visible = false;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(698, 647);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(166, 27);
+            textBox8.TabIndex = 82;
+            textBox8.Visible = false;
+            // 
+            // EkleButton
+            // 
+            EkleButton.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            EkleButton.Location = new Point(891, 511);
+            EkleButton.Name = "EkleButton";
+            EkleButton.Size = new Size(144, 52);
+            EkleButton.TabIndex = 83;
+            EkleButton.Text = "Ceza Ekle";
+            EkleButton.UseVisualStyleBackColor = true;
+            EkleButton.Visible = false;
+            EkleButton.Click += EkleButton_Click;
+            // 
+            // CezaSilButton
+            // 
+            CezaSilButton.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            CezaSilButton.Location = new Point(161, 646);
+            CezaSilButton.Name = "CezaSilButton";
+            CezaSilButton.Size = new Size(163, 50);
+            CezaSilButton.TabIndex = 84;
+            CezaSilButton.Text = "CEZA SİL";
+            CezaSilButton.UseVisualStyleBackColor = true;
+            CezaSilButton.Visible = false;
+            CezaSilButton.Click += CezaSilButton_Click;
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1087, 737);
+            Controls.Add(CezaSilButton);
+            Controls.Add(EkleButton);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(CezaSonÖdemeTarihi);
+            Controls.Add(CezaGirişTarihi);
+            Controls.Add(CezaMiktarı);
+            Controls.Add(CezaTuru);
+            Controls.Add(Plaka);
+            Controls.Add(Ilce);
+            Controls.Add(Sehir);
             Controls.Add(textBox1);
             Controls.Add(labelCezaSonOdemeTarihi);
             Controls.Add(labelCezaGirisTarihi);
@@ -209,5 +400,21 @@
         private Label labelPlaka;
         private Button SorgulaButton;
         private Label label1;
+        private Label Sehir;
+        private Label Ilce;
+        private Label Plaka;
+        private Label CezaTuru;
+        private Label CezaMiktarı;
+        private Label CezaGirişTarihi;
+        private Label CezaSonÖdemeTarihi;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button EkleButton;
+        private Button CezaSilButton;
     }
 }
--- ProjeOdev/Form3.cs
+++ ProjeOdev/Form3.cs
@@ -102,8 +102,110 @@ namespace ProjeOdev
 
         private void CezaEkle_Click(object sender, EventArgs e)
         {
+            CezaEkle.Top = 20;
+            CezaEkle.Left = 400;
 
+            CezaBilgisiOgren.Visible = false;
+            CezaSil.Visible = false;
 
+            EkleButton.Visible = true;
+            EkleButton.Location = new Point(400, 600);
+
+            Sehir.Visible = true;
+            Ilce.Visible = true;
+            Plaka.Visible = true;
+            CezaTuru.Visible = true;
+            CezaMiktarı.Visible = true;
+            CezaGirişTarihi.Visible = true;
+            CezaSonÖdemeTarihi.Visible = true;
+
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            textBox6.Visible = true;
+            textBox7.Visible = true;
+            textBox8.Visible = true;
+
+            Sehir.Location = new Point(400, 200);
+            Ilce.Location = new Point(400, 250);
+            Plaka.Location = new Point(400, 300);
+            CezaTuru.Location = new Point(400, 350);
+            CezaMiktarı.Location = new Point(400, 400);
+            CezaGirişTarihi.Location = new Point(400, 450);
+            CezaSonÖdemeTarihi.Location = new Point(400, 500);
+
+            textBox2.Location = new Point(600, 200);
+            textBox3.Location = new Point(600, 250);
+            textBox4.Location = new Point(600, 300);
+            textBox5.Location = new Point(600, 350);
+            textBox6.Location = new Point(600, 400);
+            textBox7.Location = new Point(600, 450);
+            textBox8.Location = new Point(650, 500);
+        }
+
+        private void EkleButton_Click(object sender, EventArgs e)
+        {
+            CezaBilgisi yeniCeza = new CezaBilgisi
+            {
+                Plaka = textBox4.Text,
+                CezaTuru = textBox5.Text,
+                CezaMiktari = Convert.ToDecimal(textBox6.Text),
+                CezaGirisTarihi = DateTime.Parse(textBox7.Text),
+                CezaSonOdemeTarihi = DateTime.Parse(textBox8.Text)
+
+                //CezaGirisTarihi.ToString("dd/MM/yyyy" textBox7.Text);
+            };
+
+            cezaListesi.Add(yeniCeza);
+            MessageBox.Show("Ceza eklenmiştir.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void CezaSil_Click(object sender, EventArgs e)
+        {
+            CezaSil.Top = 20;
+            CezaSil.Left = 400;
+
+            CezaBilgisiOgren.Visible = false;
+            CezaEkle.Visible = false;
+
+            label1.Visible = true;
+            textBox1.Visible = true;
+            CezaSilButton.Visible = true;
+
+            label1.Location = new Point(300, 250);
+            textBox1.Location = new Point(520, 250);
+            CezaSilButton.Location = new Point(450, 400);
+
+        }
+
+        private void CezaSilButton_Click(object sender, EventArgs e)
+        {
+            string plakaNumarasi = textBox1.Text;
+            CezaBilgisi cezaBilgisi = cezaListesi.FirstOrDefault(x => x.Plaka == plakaNumarasi);
+
+            if (cezaBilgisi != null)
+            {
+                // Cezayı listeden kaldırın
+                cezaListesi.Remove(cezaBilgisi);
+                MessageBox.Show("Seçilen ceza silinmiştir.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Kullanıcının girdiği plaka numarasını temizleyin
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Plaka numarasına ait ceza bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
+
+
